5295850ec9e08a79ec35541db3b26c7e826e414b
Jujhar <jujhar.pannu@gmail.com>
2014-11-12 14:55:57 -0800

    clean up and documentation

--- richOutput.js
+++ richOutput.js
@@ -10,22 +10,28 @@ Write a function that takes an key-value dictionary and returns the output in th
 var parseMe= {key:"value1",key2:"value2",key3:"ba"}
 var keys = Object.keys(parseMe)
 
+// Change format of dictionary to ?key=value1&key2=value2&key3=ba format
 function laParser() {
+	// always first character
     var r = "?"
+	// separting character
     var seperata = "&" 
+	
     for (var keya in keys) {
         var item = keys[keya]
         r += keys[keya]+"="+parseMe[item]+seperata
     }
+	// deleate trailing seperata variable
     r = r.substring(0, r.length - 1);
-    console.log("r is: "+r)
+    
+	console.log("r is: "+r)
     return r
 }
 
 laParser()
 
+// Test to see if match
 var output = "?key=value1&key2=value2&key3=ba"
-
 console.log(laParser()==output)
 console.log("----");
 
