467175fc43428d575f048c6416eeb20558ee42a5
Jujhar Singh <jujhar.pannu@gmail.com>
2017-10-07 23:02:23 -0700

    added es2015 files

---
+++ es2015/let.js
@@ -0,0 +1,14 @@
+let x = 2;
+var y = 2;
+
+function something(){
+    let x = 200;
+    y = y+1;
+    console.log(x + 2);
+    console.log(y + 100);
+}
+
+console.log(y + 100);
+something();
+console.log(x + 2);
+console.log(y + 100)
---
+++ es2015/promise.js
@@ -0,0 +1,8 @@
+var cutLawn = new Promise( (resolve,reject) => {
+    if (1==1){
+        resolve(console.log("Done"));
+    }
+});
+
+cutLawn.then(
+    console.log("collect money"));
